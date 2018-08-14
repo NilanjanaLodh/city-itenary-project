ba3381c8f14ca954942f3b290d26dee77c545a0a
siddharth-ag <siddharth.ag@media.net>
2018-08-14 17:41:27 +0530

    added bootstrap compatibility in settings.py

--- travelItenerary/travelItenerary/settings.py
+++ travelItenerary/travelItenerary/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'iteneraryApplication.apps.IteneraryapplicationConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
