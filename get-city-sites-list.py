3fa9a5e6160708edf86338ef09dc5b36ffa0a70a
nilanjanalodh <nilanjanalodh@gmail.com>
2018-08-16 18:46:28 +0530

    merge: get photos

---
+++ get-city-sites-list.py
@@ -0,0 +1,17 @@
+from Scrapers import CityScraper
+
+
+def main():
+    # Barcelona
+    cities = [ 'Bangkok', 'Seoul',  'Paris' 'Milan' , 'London', 'Shanghai', 'New York', 'Amsterdam', 'Istanbul',
+        'Tokyo', 'Dubai', 'Vienna', 'Kuala Lumpur',
+        'Hong Kong', 'Riyadh','Los Angeles'
+    ]
+    for city in cities:
+        print 'collecting data for {}'.format(city)
+        scraper = CityScraper(city)
+        scraper.fetch_location()
+        scraper.get_sites_list()
+        scraper.save_to_disk()
+
+main()
