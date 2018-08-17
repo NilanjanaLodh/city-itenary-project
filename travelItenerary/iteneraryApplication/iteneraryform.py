7417f41fc9478ba0fbbf5eabcbfe558716cfa0fb
siddharth-ag <siddharth.ag@media.net>
2018-08-18 01:11:42 +0530

    calendar added

--- travelItenerary/iteneraryApplication/iteneraryform.py
+++ travelItenerary/iteneraryApplication/iteneraryform.py
@@ -9,37 +9,24 @@ class IteneraryForm(ModelForm):
 		fields = ['city','start_date','end_date','type_tags']
 		widgets = {
 	      	'type_tags':forms.CheckboxSelectMultiple,
-	    	'start_date': forms.SelectDateWidget(empty_label="Nothing"),
-	    	'end_date': forms.SelectDateWidget(empty_label="Nothing"),
+	    	'start_date': forms.DateInput(attrs={'class':'datepicker'}),
+	    	'end_date': forms.DateInput(attrs={'class':'datepicker'}),
 	    }
-
-	def clean_start_date(self):
-		cleaned_data = super(IteneraryForm, self).clean()
-		today_date = datetime.date.today()
-		start_date = cleaned_data.get("start_date")
-
-		if start_date<today_date:
-			raise forms.ValidationError("the start date is too early")
-
-		return start_date
-
-	def clean_end_date(self):
-		cleaned_data = super(IteneraryForm, self).clean()
-		today_date = datetime.date.today()
-		end_date = cleaned_data.get("end_date")
-
-		if end_date<today_date:
-			raise forms.ValidationError("the end date is too early")
-
-		return end_date
 
 	def clean(self):
 		cleaned_data = super(IteneraryForm, self).clean()
-		start_date = self.cleaned_data.get("start_date")
+		start_date = cleaned_data.get("start_date")
+		today_date = datetime.date.today()
 		end_date = cleaned_data.get("end_date")
 
-		if start_date>end_date:
-			raise forms.ValidationError("The start date cannot be after end date")
+		if start_date and end_date:
+			if start_date<today_date:
+				raise forms.ValidationError("the start date is too early")
+			if end_date<today_date:
+				raise forms.ValidationError("the end date is too early")
+
+			if start_date>end_date:
+				raise forms.ValidationError("The start date cannot be after end date")
 
 		# if end_date<today_date:
 		# 	raise forms.ValidationError("the end date is too early")
--- travelItenerary/travelItenerary/settings.py
+++ travelItenerary/travelItenerary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'iteneraryApplication',
     'bootstrap3',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
