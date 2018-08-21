fa97de9a3d258ed8cc137c8adcdc9fec17a3dbd3
siddharth <siddharth.ag@media.net>
2018-08-21 13:01:34 +0530

    poi.py changed

--- travelItenerary/poi.py
+++ travelItenerary/poi.py
@@ -15,24 +15,21 @@ for root, dirs, files in os.walk(os.path.abspath(os.path.join(os.getcwd(), os.pa
         if "info" in file_name:
             with open(file_name) as data:
                 json_data = json.load(data)
-                if "rating" in json_data:
-                    new_poi = PointOfInterest()
-                    new_poi.POI_id = os.path.dirname(file_name).split('/')[-1]
-                    new_poi.POI_name = json_data['name']
-                    new_poi.latitude = json_data['lat']
-                    new_poi.longitude = json_data['lng']
-                    new_poi.rating = json_data['rating']
+
+                new_poi = PointOfInterest()
+                new_poi.POI_id = os.path.dirname(file_name).split('/')[-1]
+                new_poi.POI_name = json_data['name']
+                new_poi.latitude = json_data['lat']
+                new_poi.longitude = json_data['lng']
+                try:
                     new_poi.POI_city = City.objects.get(city_name = os.path.dirname(file_name).split('/')[-3])
-                    if new_poi.POI_city.city_name == "Shanghai": 
-                        print ">>>>>>" + new_poi.POI_id
+                    try: new_poi.rating = json_data['rating']
+                    except: new_poi.rating = 2.5
+                    
                     new_poi.save()
                     
                     poi_types = json_data['types']
                     for poi_type in poi_types:
                         new_poi.types.add(poi_type)
 
-
-
-
-
-
+                except: pass
